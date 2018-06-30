5a4cca35cb69d72cada6ec76c751a9865b7e6b26
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2018-06-30 23:55:45 +0900

    [minor] Fix Access-Control-Allow- thing header

--- firebase-auth_demo/app.js
+++ firebase-auth_demo/app.js
@@ -17,9 +17,9 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:9000");
+  res.header('Access-Control-Allow-Origin', req.headers['origin']);
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
 
